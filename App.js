b5ab2d47dc22a988a053d745a7ae34ebb9d61d4b
swikriti-igp <swikriti.jaiswal@joinventures.com>
2026-02-26 11:57:27 +0530

    Igniting out App

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
- const heading = React.createElement('h1', {'id':'heading'}, 'Hello React');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+// root.render(heading);
 
 
 // Nested Elements code start here
 const parent = React.createElement('div',{'id':'parent'},[
-    React.createElement("div",{"id":"child1"},[
-        React.createElement("h1",{"id": "heading1","className":'abc'},"Heading 1"),
-        React.createElement("h2",{"id": "heading2"},"Heading 2")
+    React.createElement("div",{"id":"child1",'key':'child1'},[
+        React.createElement("h1",{"id": "heading1","className":'abc','key':'heading1'},"Heading 1"),
+        React.createElement("h2",{"id": "heading2","key":"heading2"},"Heading 2")
     ]),
-    React.createElement("div",{"id":'child2'},[
-        React.createElement("h1",{"id": "heading3"},"Heading 3"),
-        React.createElement("h2",{"id": "heading4"},"Heading 4")
+    React.createElement("div",{"id":'child2','key':'child2'},[
+        React.createElement("h1",{"id": "heading3","key":"heading3"},"Heading 3"),
+        React.createElement("h2",{"id": "heading4","key":"heading4"},"Heading 4")
     ])
 ]);
 
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Hey React 👋
 
 **Hey React** by **Swikriti** is a fun and interactive React project to showcase my frontend skills.
+
+
+#Parcel 
+- offical document: https://parceljs.org/
+- dev build
+- local server
+- HMR - Hot Module Replacement
+- File Watching Alogorithm - Written in C++
+- caching - faster build
+- Image Optimization
+- compress the file ( to shipped on Production)
+- Bundling
+- consistent Hashing
+- code splitting
+- Differential Bundling - support older browser
+- Error Handling
+- Beautiful Diagonstic
+- HTTPs
+- Tree Shaking - remove unsed code
